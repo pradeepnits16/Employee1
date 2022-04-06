f86fc243a65f2347b2c4854c7aba2144381df1b9
prade <prade@LAPTOP-9GU8O6EO>
2022-04-06 23:41:35 +0530

    commit 2

--- src/main/java/com/example/demo/Employee1Application.java
+++ src/main/java/com/example/demo/Employee1Application.java
@@ -8,6 +8,8 @@ public class Employee1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Employee1Application.class, args);
+		
+		System.out.println("hi I am only available in branch");
 	}
 
 }
